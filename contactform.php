b7a44798f532b2719d4f7120eea5c99e5e50b8c0
JulianGrafen <juliangraefen@gmail.com>
2022-05-27 11:35:41 +0200

    Added PHP for contactform

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $mailFrom = $_POST['mail'];
+
+    $mailTo ="projekt.vencio@gmail.com";
+    $headers ="From: ".$mailFrom;
+    $txt = "E-Mail from".$name.".\n\n".$message;
+
+
+    mail($mailTo,$txt,$headers);
+    header("Location: index.php?mailsend");
+}
